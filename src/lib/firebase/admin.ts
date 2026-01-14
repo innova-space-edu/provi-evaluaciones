8bb0827d1c664880fcf35444681c32215f3ac70b
Innova Space <emorales@colprovidencia.cl>
2026-01-14 20:50:25 -0300

    Update admin.ts

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -1,13 +1,19 @@
 import "server-only";
 import admin from "firebase-admin";
 
-if (!admin.apps.length) {
-  const projectId = process.env.FIREBASE_PROJECT_ID!;
-  const clientEmail = process.env.FIREBASE_CLIENT_EMAIL!;
-  const privateKey = process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n");
+function getPrivateKey() {
+  const key = process.env.FIREBASE_PRIVATE_KEY;
+  if (!key) return undefined;
+  return key.replace(/\\n/g, "\n");
+}
 
+if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert({ projectId, clientEmail, privateKey }),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: getPrivateKey()
+    } as any)
   });
 }
 
